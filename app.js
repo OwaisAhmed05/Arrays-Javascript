78511449c7c190736ec2e37dc611ba394085694d
Owais <owaisbusiness97@gmail.com>
2024-03-05 22:49:51 +0500

    Learning Array Javascript

---
+++ app.js
@@ -0,0 +1,70 @@
+// for (var index = 0; index < 5; index++) {
+//     document.write("hello world");
+// }
+
+// var arr = ["karachi", "lahore", "islamabad", "pindi"]
+// var cityName = prompt("Enter city name: ");
+// var cityFound = false;
+// for (var i = 0; i < arr.length; i++) {
+
+//     if (cityName == arr[i]) {
+//         cityFound = true
+//         alert("yes this city is one of the cleanest cities");
+//         break
+//     }
+// }
+// if (cityFound == false) {
+//     alert("city not found")
+// }
+
+
+
+
+
+// initialization
+// condition
+// inc / dec
+
+
+// var num = 5
+// num = num + 2
+// num += 4
+
+
+
+
+// var i;
+
+// for(i=0; i<=10; i = i+2){
+//     document.write(i+". smit"+"<br>")
+// }
+
+
+// "For Reverse counting" //
+
+// for(var i=10; i > 0; i--){
+//     document.write(i, "<br>")
+// }
+
+
+
+// Print Table Using "for" Loop From User Input
+
+// var i;
+// var inp = +prompt("Enter number to print table")
+// var inp2 = +prompt("Enter table length")
+
+
+// for (i = 1; i <= inp2; i=i+2) {
+//     document.write(inp + " x " + i + " = " + inp * i + "<br>")
+// }
+
+
+
+
+
+
+
+
+
+
